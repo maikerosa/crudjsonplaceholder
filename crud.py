fb8df3026fe3744af74e7ee6ae33a91954eb6e75
maikerosa <maikeofc18@gmail.com>
2022-10-13 16:33:01 -0300

    ac

--- crud.py
+++ crud.py
@@ -2,7 +2,9 @@ from time import sleep
 import requests
 
 global api_url
+global api_url_todos
 api_url = "https://jsonplaceholder.typicode.com/users"
+api_url_todos = "https://jsonplaceholder.typicode.com/todos"
 
 class crud:
     def __init__(self):
@@ -68,6 +70,7 @@ class crud:
                 print('Tarefa criada! ', self.create_task(user_id, title, completed))
             elif op == 7:
                 task_id = input('Digite o ID da tarefa que deseja atualizar: ')
+                print('Tarefa selecionada: ', self.name_task(task_id))
                 title = input('Digite o título da tarefa: ')
                 completed = input('Digite o status da tarefa (true/false): ')
                 print('Tarefa atualizada! ', self.update_task(task_id, title, completed))
@@ -84,25 +87,25 @@ class crud:
         return requests.post(api_url + "/" + str(user_id) + "/todos", data={"title": title, "completed": completed}).json()
 
     def update_task(self, task_id, title, completed):
-        return requests.put(api_url + "/" + str(task_id), data={"title": title, "completed": completed}).json()
+        return requests.put(api_url_todos + "/" + str(task_id), data={"title": title, "completed": completed}).json()
 
     def list_user_tasks(self, user_id):
         return requests.get(api_url + "/" + str(user_id) + "/todos").json()
 
-    def list_todo(self, user_id):
+    def list_todo(self, user_id=None, task_id=None):
         return requests.get(api_url + "/" + str(user_id) + "/todos").json()
 
     def create_user(self, name, username, email):
         return requests.post(api_url, data={"name": name, "username": username, "email": email}).json()
 
     def update_user(self, user_id, name, username, email):
-        return requests.put(api_url + "/" + str(user_id), data={"name": name, "username": username, "email": email}).json()
+        return requests.put("api_url" + "/" + str(user_id), data={"name": name, "username": username, "email": email}).json()
 
     def delete_user(self, user_id):
         return requests.delete(api_url + "/" + str(user_id)).status_code
 
     def delete_task(self, task_id):
-        return requests.delete(api_url + "/" + str(task_id)).status_code
+        return requests.delete(api_url_todos + "/" + str(task_id)).status_code
 
     def list_all_users(self, print_users = True):
         list = requests.get(api_url).json()
@@ -110,6 +113,9 @@ class crud:
             for user in list:
                 print(user['name'])
         return list
+
+    def name_task(self, task_id):
+        return requests.get(api_url_todos + "/" + str(task_id)).json()['title']
 
 if __name__ == "__main__":
     new_crud = crud()
